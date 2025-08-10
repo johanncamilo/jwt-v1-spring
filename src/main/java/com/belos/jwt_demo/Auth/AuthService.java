734547db7cd47b34527056e803bdaa95ad12a0a6
Johann mac <milodevcool@gmail.com>
2025-08-09 20:07:55 -0500

    Security config + requests

--- src/main/java/com/belos/jwt_demo/Auth/AuthService.java
+++ src/main/java/com/belos/jwt_demo/Auth/AuthService.java
@@ -1,5 +1,8 @@
 package com.belos.jwt_demo.Auth;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
     
 }
--- src/main/java/com/belos/jwt_demo/Config/SecurityConfig.java
+++ src/main/java/com/belos/jwt_demo/Config/SecurityConfig.java
@@ -1,5 +1,28 @@
 package com.belos.jwt_demo.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // cadena de filtros
+        return http
+            .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para simplificar la configuración
+            .authorizeHttpRequests(authRequest -> authRequest
+                .requestMatchers("/auth/**").permitAll() // Permitir acceso a las rutas de autenticación
+                .anyRequest().authenticated() // Requiere autenticación para cualquier otra solicitud
+            ).formLogin(withDefaults()) // Configuración de inicio de sesión por defecto
+            .build();
+    }
 }
